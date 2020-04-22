ca4781a6ce6c74ecbb0901cc60296e8a16b497ce
nCally <cartrf@live.com>
2020-04-22 23:47:38 +0100

    work on earshot

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class EarshotApp extends StatelessWidget {
           primaryColor: const Color(0XFF2B3B46),
           accentColor: const Color(0XFFCE9073),
           textTheme: TextTheme(
-              body1: TextStyle(fontSize: 18, color: const Color(0XFFCDBFBC)),
+              body1: TextStyle(fontSize: 36, color: const Color(0XFFCDBFBC)),
               body2: TextStyle(fontSize: 18, color: const Color(0XFFCDBFBC)))),
       home: HomeScreen(),
     );
@@ -25,37 +25,83 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _currentTab = 0;
+  bool _current = true;
 
-  void _switchTabs() {
-    if (_currentTab == 0) {
-      _currentTab = 1;
-    } else {
-      _currentTab = 0;
-    }
-
+  void setCurrent() {
     setState(() {
-      _currentTab = _currentTab;
+      _current = !_current;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).primaryColor,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Text(
-                'What nostrud et.',
-                style: TextStyle(fontSize: 18),
-              )
-            ],
-          )
-        ],
+    return Scaffold(
+      body: Container(
+        color: Theme.of(context).primaryColor,
+        child: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Padding(
+              padding: EdgeInsets.only(top: 115.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Opacity(
+                            opacity: _current == true ? 1.0 : 0.3,
+                            child: Container(
+                              child: Text('Recent',
+                                  style: Theme.of(context).textTheme.body1),
+                            ),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Padding(
+                              padding: EdgeInsets.only(left: 20.0),
+                              child: Opacity(
+                                opacity: _current == true ? 0.3 : 1.0,
+                                child: Container(
+                                  child: Text('Library',
+                                      style: Theme.of(context).textTheme.body1),
+                                ),
+                              ))
+                        ],
+                      )
+                    ],
+                  ),
+                  Container(
+                    child: _currentChild(),
+                  )
+                ],
+              ),
+            )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _current = !_current;
+          });
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
       ),
     );
+  }
+
+  Widget _currentChild() {
+    if (_current == true) {
+      return Container(
+        child: ListView(
+          children: <Widget>[Text('True screen')],
+        ),
+      );
+    } else {
+      return Text('Second, false screen');
+    }
   }
 }
